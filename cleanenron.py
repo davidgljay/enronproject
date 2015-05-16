72b55ea04fe0e0c4c9fcdbe97222fc89438424bf
David Jay <davidjay@samsungaccelerator.com>
2015-05-16 15:59:30 -0400

    Setting file to read in commandline.

--- cleanenron.py
+++ cleanenron.py
@@ -1,34 +1,40 @@
 from dateutil.parser import *
 import re
+import sys
 
-inputfile = open("sentitems", "r")
+if (len(sys.argv)<1 or len(sys.argv)>2):
+	print "You must enter exactly one file to scan for email."
+	sys.exit()
+else:
+	ifile = sys.argv[1]
+counter = 0
+inputfile = open(ifile, "r")
 outputfile = open("enronemails.csv", "a")
+
 vals={}
 for line in inputfile:
 	splitline = line.split(":")
-	if (splitline[0].lower() == "from"):
+	if (splitline[0].lower() == "from" and not "from" in vals):
 		splitline.pop(0)
 		vals["from"]=":".join(splitline).rstrip()
-	elif (splitline[0].lower() == "to"):
+	elif (splitline[0].lower() == "to" and not "to" in vals):
 		splitline.pop(0)
 		vals["to"]=":".join(splitline).rstrip()
-	elif (splitline[0].lower() == "cc"):
+	elif (splitline[0].lower() == "cc" and not "cc" in vals):
 		splitline.pop(0)
-		vals["cc"]=":"join(splitline).rstrip()
-	elif (splitline[0].lower() == "bcc"):
+		vals["cc"]=":".join(splitline).rstrip()
+	elif (splitline[0].lower() == "bcc" and not "bcc" in vals):
 		splitline.pop(0)
 		vals["bcc"]=":".join(splitline).rstrip()
-	elif (splitline[0].lower() == "date"):
+	elif (splitline[0].lower() == "date" and not "date" in vals):
 		splitline.pop(0)
 		datelist = ":".join(splitline).split(")")
 		if (len(datelist)>1):
 			datestring = ":".join(splitline).split(")")[0].rstrip().lstrip() + ")"
 		else:
 			datestring = ":".join(splitline).split(")")[0].rstrip().lstrip()
-
-		print "Date: " + datestring
 		vals["date"]=parse(datestring).isoformat()
-	elif (splitline[0].lower() == "subject"):
+	elif (splitline[0].lower() == "subject" and not "subject" in vals):
 		splitline.pop(0)
 		vals["subject"]=":".join(splitline).rstrip().lstrip()
 	if ("to" in vals and "from" in vals and "date" in vals and "subject" in vals):
@@ -41,3 +47,5 @@ for line in inputfile:
 		outputline = vals["date"] + '|' + vals["from"] + '|' + vals["to"] + '|' + cc + '|' + bcc + '|"' + vals["subject"] + '"\n'
 		outputfile.write(outputline)
 		vals={}
+		counter+=1
+print "Added " + str(counter) + " emails to enronemaiils.csv"
